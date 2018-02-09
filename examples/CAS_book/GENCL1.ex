2756e2b76023a0d4ef6a40c95e5318ebe46a3deb
Krishna Myneni <krishna.myneni@ccreweb.org>
2018-02-09 10:45:59 -0600

    Develop (#12)
    * Added examples/CAS_book/GENCL1.ex; revised examples/CAS_book/NONH.ex to generate proper cfg.inp file; removed unneeded cfg.inp.example file

---
+++ examples/CAS_book/GENCL1.ex
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Generate the possible angular momentum couplings for a
+# 3P term, with the electron configuration:
+#
+#    1s(2)2s(1)2p(2)3s(1)
+#
+# Coupling within the subshells leads to the four LS terms
+# (see the program TERMS and CAS Table 2.1):
+#
+#   1S 2S 3P 2S
+#
+# Each of these may be coupled together in the following ways
+# to produce a 3P term:
+#
+#   1) 1S + 2S --> 2S + 3P --> 2P + 2S --> 3P
+#
+#   2) 1S + 2S --> 2S + 3P --> 4P + 2S --> 3P
+#
+# This example is given in Table 2.2 of CAS [1].
+#
+# References:
+#   1. Computational Atomic Structure: An MCHF Approach,
+#      C.F. Fischer, T. Brage, and P. Jonsson, IoP Publishing
+#      (2000).
+
+BIN=../../bin
+
+rm -f cfg.inp
+$BIN/GENCL <<S1
+
+Example
+1s
+2s(1)2p(2)3s(1)
+
+
+
+3P
+
+S1
+
+# Display the couplings found by GENCL 
+cat cfg.inp
+
+
+
